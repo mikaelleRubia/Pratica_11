96ad900528d0bb636232a9a0544d733415aafb4c
mikaelleRubia <mikaelle.rubia@outlook.com>
2023-09-12 22:55:51 -0300

    feature: funcao de codificar e descodificar strings

--- exercicio11.cpp
+++ exercicio11.cpp
@@ -3,17 +3,61 @@
 
 using namespace std;
 
+string codificarcao (string frase){
+    string frase_nova ="";
+    for (char x: frase){
+        if(isalpha(x)){
+            if(x == 'Z'){
+                x= 'A';
+            }
+            else if(x == 'z'){
+                   x='a'; 
+            }
+            else{
+                x++;
+                frase_nova += x;
+            }
+        }else{
+            frase_nova += " ";
+
+        }
+    }
+    return frase_nova;
+}
+
+
+
+string descodificarcao (string frase){
+    string frase_nova ="";
+    for (char x: frase){
+        if(isalpha(x)){
+            if(x == 'Z'){
+                x= 'A';
+            }
+            else if(x == 'z'){
+                   x='a'; 
+            }
+            else{
+                x--;
+                frase_nova += x;
+            }
+        }else{
+            frase_nova += " ";
+        }
+    }
+    return frase_nova;
+}
+
 int main() {
 
-    string nome;
-    cout << "digite um nome:" << endl;
-    getline(cin, nome);
+    string nome ="Estrutura de Dados";
+    string frase_codificada ="";
 
-    for (int i = 0; i < nome.length(); i++) {
-            cout << nome.at(i) << endl;
+    frase_codificada = codificarcao(nome);
+    cout << "codificada: " <<frase_codificada << endl;
 
-    }
+    frase_codificada = descodificarcao(frase_codificada);
+    cout << "descodificada: " << frase_codificada << endl;
 
     return 0;
 }
-
