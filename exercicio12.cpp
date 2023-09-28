5c4f183ca1d88aa38766cf3a00ecea3da177c198
mikaelleRubia <mikaelle.rubia@outlook.com>
2023-09-27 21:50:47 -0300

    fix: logica, para strings com tamanhos diferentes

--- exercicio12.cpp
+++ exercicio12.cpp
@@ -4,27 +4,38 @@
 
 using namespace std;
 
-void compara_string(string nome1, string nome2,string nome){
+void compara_string(string nome1, string nome2){
+    int tam1 = nome1.size();
+    int tam2 = nome2.size();
+    string nome;
+    string  tamanho_vets;
+    tamanho_vets.resize(tam1 + tam2);
 
-    for (auto x: nome1) 
-    for (auto y: nome2)
-
-    if(x == y){
-        nome +=x;
+    int i = 0, j = 0, k = 0;
+    while (i < tam1 && j < tam2) {
+        tamanho_vets[k++] = nome1[i++];
+        tamanho_vets[k++] = nome2[j++];
     }
 
-    cout << " letra: " << nome << endl;
+    while (i < tam1) {
+        tamanho_vets[k++] = nome1[i++];
+    }
+
+    while (j < tam2) {
+        tamanho_vets[k++] = nome2[j++];
+    }
+
+    for (int i = 0; i < tam1 + tam2; i++) {
+        cout << tamanho_vets[i] << " ";
+    }
 }
 
 int main()
 {
     string nome_a = "CASDWQTR";
     string nome_b = "TRSAY";
-    string nome= "";
 
-
-
-    compara_string( nome_a,nome_b, nome);
+    compara_string( nome_a,nome_b);
 
     return 0;
 }
